359c6af3aa3f1085dc5127b55c00f3b5b0bf8f81
hagay cohen <hagaico100@gmail.com>
2022-01-31 10:23:49 +0200

    end game

--- game.js
+++ game.js
@@ -4,37 +4,59 @@ const userClickedPattern = [];
 let start = false;
 let gameOver = false;
 let level = 0;
+let levelUp = 0;
 let userClick = 0;
+let USER_LEVEL = 2;
+
+if (typeof(Storage) !== "undefined") {
+    if(localStorage.getItem("topLevel") !== null){
+    $("#topLevel").text("השיא כרגע הוא: "+ localStorage.getItem("topLevel")); 
+}else{
+    localStorage.setItem("topLevel", "0");
+    $("#topLevel").text("השיא כרגע הוא: "+ localStorage.getItem("topLevel")); 
+}
+}
+
 // for start game:
+
 runGame();
     
-$(".btn").click(function(){
+$(".btnG").click(function(){
    let userChosenColour = this.id;
    userClickedPattern.push(userChosenColour);
-   console.log(userClickedPattern);
-   console.log(gamePattern);
-
 
    playSound(userChosenColour);
    animatePress(userChosenColour);
-
    checkAnswer(userClick);
    userClick++;
 
-    //console.log("userClick:",userClick);
-   // console.log("game gamePattern.length:",gamePattern.length);
-
-
 if(userClick===gamePattern.length){
-    console.log("in if");
-userClick = 0;
-if(gameOver===false){
+    level++
+        if(level % USER_LEVEL == 0){
+            levelUp++;
+        $("#level-title").text("שלב "+ levelUp); 
+        playSound("applause");
 
-    previousSequence(gamePattern);    
+  //update in localStorage:      
+if (typeof(Storage) !== "undefined") {
+    if(localStorage.getItem("topLevel") < levelUp){
+localStorage.setItem("topLevel", levelUp);
+$("#topLevel").text("השיא כרגע הוא: "+ localStorage.getItem("topLevel")); 
+
+}
+}   
+    }
+
+    userClick = 0;
     
+    if(gameOver===false){    
+    setTimeout(function(){
+        previousSequence(gamePattern);    
+    },(1000));
+
     setTimeout(function(){
     nextSequence();    
-    },(1000*gamePattern.length));
+    },(1000*gamePattern.length+1000)); //the 1000 in end for wait until the previousSequence end.
 }
 }
    })
@@ -46,16 +68,15 @@ function nextSequence(){
     
    $("#"+ randomChosenColour).fadeIn(300).fadeOut(200).fadeIn(300);
    playSound(randomChosenColour);
-   level++
-   $("#level-title").text("שלב "+ level); 
-
+   //
    userClickedPattern.length = 0;
 }
 
 function previousSequence(arr){
 for(let i=0; i<arr.length; i++){    
     setTimeout(function(){
-    $("#"+ arr[i]).fadeIn(300).fadeOut(200).fadeIn(300);    
+    $("#"+ arr[i]).fadeIn(300).fadeOut(200).fadeIn(300);
+    playSound(arr[i]);    
 },(1000*i))
 }
 }
@@ -78,22 +99,31 @@ function checkAnswer(click){
     if(gamePattern[click] != userClickedPattern[click]){
             gameOver = true;
             $("body").addClass("game-over");
+            $(".btnG").attr("hidden", "hidden");
             playSound("wrong");
             $("#level-title").text("המשחק נגמר, נסה שוב"); 
             $("#playAgain").removeAttr("hidden");
-         }else{
-            
+            $(".nav").removeAttr("hidden");
+
          }
 }
+
+function chooseLevelClick(ChooseLevel){
+    USER_LEVEL = ChooseLevel;
+}
+
 function runGame(){
 $("#playAgain").attr("hidden", "hidden");
 $("#level-title").text("הקש על אחד המקשים כדי להתחיל"); 
 $("body").removeClass("game-over");
+$(".btnG").removeAttr("hidden");
+
 
  gamePattern.length=0;
  userClickedPattern.length =0;
  gameOver = false;
  level = 0;
+ levelUp = 0;
  userClick = 0;
  start= false;
 
@@ -101,8 +131,11 @@ $(document).keypress(function() {
     if(!start){ //run only one time:
     console.log( "Handler for .keypress() called." );
     start = true;
-    $("#level-title").text("שלב "+ level); 
+    $("#level-title").text("שלב "+ levelUp);
+    $(".nav").attr("hidden", "hidden"); 
         nextSequence();
     }
     });
-}
+}   
+
+
